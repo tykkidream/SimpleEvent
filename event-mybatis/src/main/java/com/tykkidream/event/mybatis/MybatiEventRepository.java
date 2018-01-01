4a929fabc69035f8733c7cad3eac7132457203e8
Tykkidream <tykkidream@gmail.com>
2018-01-01 16:48:26 +0800

    添加event-mybatis项目

---
+++ event-mybatis/src/main/java/com/tykkidream/event/mybatis/MybatiEventRepository.java
@@ -0,0 +1,16 @@
+package com.tykkidream.event.mybatis;
+
+import com.tykkidream.event.core.Event;
+import com.tykkidream.event.core.EventRepository;
+
+import java.util.List;
+
+/**
+ * Created by Tykkidream on 2018/1/1.
+ */
+public class MybatiEventRepository implements EventRepository {
+    @Override
+    public void save(List<Event> events) {
+
+    }
+}
